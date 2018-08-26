c24280d851533e1145d577677cf5f05d586d6173
Andrei Lapshin <alapshin@fastmail.com>
2018-08-26 09:38:36 +0300

    Rewrite comment for custom checkout step

--- vars/customCheckout.groovy
+++ vars/customCheckout.groovy
@@ -2,15 +2,19 @@ import hudson.plugins.git.GitSCM
 
 // Checkout step with custom behavior
 //
-// This is done to make Jenkins use value of "Author" field from git commits in build changelog instead of "Committer"
-// field. This way  we get correct changelog after rebase or cherry-pick.
-// Jenkins' documentation states that enabling this custom behavior disables faster polling implementation (based on
-// git ls-remote) but since we don't use polling this should be acceptable trade-off.
+// This is done to force Jenkins use value of "Author" field from git commits 
+// in changelog instead of "Committer" field. 
+// This way  we can get correct changelog after rebase or cherry-pick.
+// Jenkins' documentation states that enabling this custom behavior disables 
+// faster polling implementation (based on git ls-remote) however this is no 
+// longer the case. For detail see  
+// https://github.com/jenkinsci/git-plugin/commit/04152e98a19da6c43658a22c3f3340a26ac2fb49
 //
 // For details about how to customize checkout behavior see
 // https://support.cloudbees.com/hc/en-us/articles/226122247-How-to-Customize-Checkout-for-Pipeline-Multibranch-
 //
-// To avoid manual whitelisting of access to scm.* attributes this is implemented as custom step in shared library.
+// This is implemented as custom step in shared library to avoid whitelisting 
+// of access to scm.* attributes.
 
 def call(GitSCM scm) {
     checkout([
